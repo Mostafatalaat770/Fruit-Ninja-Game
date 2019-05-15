8f39b9f1dd28848eef6df46da31260a5c3817d7d
ahmed-swidan <48591254+ahmed-swidan@users.noreply.github.com>
2019-05-15 09:22:06 +0200

    added a timer when you click play button to make the player ready

--- src/Gui/Controller/Classic.java
+++ src/Gui/Controller/Classic.java
@@ -8,21 +8,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
-import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.util.Duration;
-import org.w3c.dom.css.Rect;
-
-import java.awt.event.ActionEvent;
-import java.awt.geom.Rectangle2D;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Classic implements Initializable {
    private Controller controller= new Controller();
     @FXML private Canvas canvas ;
+    @FXML ImageView pause;
+    @FXML ImageView resume;
     private GraphicsContext gc;
     private Image image = new Image("Resources/ConceptGreatWave1 (2).jpg",1280,720,false,false);
 
@@ -31,6 +32,7 @@ public class Classic implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         controller.type = "classic";
         controller.lives = 3;
+        resume.setVisible(false);
         gc=canvas.getGraphicsContext2D();
         gc.drawImage(image, 0, 0);
 
@@ -78,17 +80,51 @@ public class Classic implements Initializable {
         };
         timer.start();
 
+        pause.setOnMouseClicked(event -> {
+            pause.setVisible(false);
+            resume.setVisible(true);
+            timeline.stop();
+            timer.stop();
+            clock.stop();
+            canvas.setEffect(new GaussianBlur(50));
+        });
+
+        resume.setOnMouseClicked(event -> {
+            if(resume.isVisible()){
+                resume.setVisible(false);
+                canvas.setEffect(new GaussianBlur(-50));
+                AtomicInteger seconds= new AtomicInteger();
+                Timeline resume = new Timeline(new KeyFrame(new Duration(500), acttionEvent->{
+                    controller.drawAllThings(gc,controller.throwables,controller);
+                    seconds.getAndIncrement();
+                    gc.setFill(Color.ORANGE);
+                    gc.setStroke(Color.BLACK);
+                    gc.setLineWidth(5);
+                    Font theFont = Font.font("Gang Of Three", 200);
+                    gc.setFont(theFont);
+                    gc.fillText(String.valueOf(4-seconds.get()), 600, 350);
+                    gc.strokeText(String.valueOf(4-seconds.get()), 600, 350);
+                }));
+                resume.setCycleCount(4);
+                resume.play();
+                resume.setOnFinished(event1 -> {
+                    pause.setVisible(true);
+                    timeline.play();
+                    timer.start();
+                    clock.play();
+                });
+
+            }
+        });
+
 
     }
 
     public void handleMove(MouseEvent event){
-        if(controller.isSliced(controller.throwables,event.getSceneX(),event.getSceneY(),controller)) {
-            System.out.println("cut");
-            // todo: cut
+        controller.slice(controller.throwables,event.getSceneX(),event.getSceneY(),controller);
+
         }
 
-    }
-
-
-
 }
+
+
--- src/Gui/Controller/Controller.java
+++ src/Gui/Controller/Controller.java
@@ -4,6 +4,7 @@ import Interfaces.Factory.ObjectCreator;
 import Interfaces.GameActions;
 import Interfaces.GameObject;
 import Throwables.Bombs.Bomb;
+import Throwables.Fruits.Fruit;
 import Throwables.Fruits.SpecialFruits.SpecialFruit;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
@@ -20,12 +21,12 @@ import java.util.Random;
 public class Controller implements GameActions {
     private static Controller ourInstance = new Controller();
     public ArrayList<GameObject> throwables= new ArrayList<>();
-    public int Score = 0;
-    public int secs  =  0;
-    public  int mins = 0;
-    public int lives;
-    public int difficulty = 1;
-    public String type;
+    private int Score = 0;
+    int secs  =  0;
+    int mins = 0;
+    int lives;
+    int difficulty = 1;
+    String type;
 
     public static Controller getInstance() {
         return ourInstance;
@@ -76,17 +77,14 @@ public class Controller implements GameActions {
            GameObject throwable= iterator.next();
            if(throwable.hasMovedOffScreen() && throwable.isSliced()){
                iterator.remove();
-               System.out.println("removed");
                return true;
            }
            else if (throwable.hasMovedOffScreen() && !throwable.isSliced() && !(throwable instanceof Bomb)) {
                iterator.remove();
-               System.out.println("removed");
                 return false;
             }
           else if(throwable.hasMovedOffScreen()){
                iterator.remove();
-               System.out.println("removed");
                return true;
            }
         }
@@ -113,35 +111,35 @@ public class Controller implements GameActions {
         gc.fillText("best score: ",20,50);
 
 
-
-        for(int i=0;i<list.size();i++){
-            list.get(i).render(gc);
-            list.get(i).updatePosition();
+        for (GameObject gameObject : list) {
+            gameObject.render(gc);
+            gameObject.updatePosition();
         }
     }
 
-    public boolean isSliced(ArrayList<GameObject> throwables,double x, double y,Controller controller){
-        for(int i=0;i<throwables.size();i++){
-            if(x>throwables.get(i).getXlocation()&&x<throwables.get(i).getXlocation()+throwables.get(i).getImages()[0].getWidth()){
-                if(y>throwables.get(i).getYlocation()&&y<throwables.get(i).getYlocation()+throwables.get(i).getImages()[0].getHeight()){
-                    if (!throwables.get(i).isSliced()) {
-                        throwables.get(i).setFalling(true);
-                        throwables.get(i).setSliced(true);
-                        controller.Score++;
-                        if(throwables.get(i) instanceof Bomb){
+    public void slice(ArrayList<GameObject> throwables,double x, double y,Controller controller){
+        for (GameObject throwable : throwables) {
+            if (x > throwable.getXlocation() && x < throwable.getXlocation() + throwable.getImages()[0].getWidth()) {
+                if (y > throwable.getYlocation() && y < throwable.getYlocation() + throwable.getImages()[0].getHeight()) {
+                    if (!throwable.isSliced()) {
+                        throwable.setFalling(true);
+                        throwable.setSliced(true);
+                        if (throwable instanceof Fruit) {
+                            controller.Score++;
+                            // todo slice sound
+                        }
+                        if (throwable instanceof Bomb) {
                             // todo : special effects
-                            controller.Score--;
+                            // todo slice sound (bomb)
                         }
-                        if(throwables.get(i) instanceof SpecialFruit){
+                        if (throwable instanceof SpecialFruit) {
                             //todo : special effects
-                            controller.Score--;
+                            // todo slice sound (special fruit)
                         }
-                        return true;
                     }
                 }
             }
         }
-        return false;
     }
 
 
