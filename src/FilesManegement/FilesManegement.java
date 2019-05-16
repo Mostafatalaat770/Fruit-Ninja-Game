965056a7e1814bbed206e0d9d35a771afcc8a574
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-05-16 23:09:25 +0200

    Files: Added multiple users support and fixed bugs
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/FilesManegement/FilesManegement.java
+++ src/FilesManegement/FilesManegement.java
@@ -1,5 +1,6 @@
 package FilesManegement;
 
+import Gui.Controller.Controller;
 import Interfaces.GameObject;
 import Throwables.Bombs.DangerousBomb;
 import Throwables.Bombs.FatalBomb;
@@ -19,7 +20,6 @@ import org.jdom2.output.XMLOutputter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,38 +32,65 @@ public class FilesManegement {
         return ourInstance;
     }
 
-    private List<GameObject> throwables = new ArrayList<>();
-    private int numberOfLives;
-    private int difficulty;
-    private int score;
-    private int time;
-    private int highScore;
+    public GameObject createObject(String type) {
+        GameObject object = null;
+        switch (type) {
+            case "apple":
+                object = new Apple();
+                break;
+            case "banana":
+                object = new Banana();
+                break;
+            case "melon":
+                object = new Melon();
+                break;
+            case "orange":
+                object = new Orange();
+                break;
+            case "freezeBanana":
+                object = new FreezeBanana();
+                break;
+            case "magicBeans":
+                object = new MagicBeans();
+                break;
+            case "dangerousBomb":
+                object = new DangerousBomb();
+                break;
+            case "fatalBomb":
+                object = new FatalBomb();
+                break;
+        }
+        return object;
+    }
 
+    public void save(Controller controller) throws IOException {
 
-    public void save(List<GameObject> throwables, String type, int numberOfLives, int difficulty, int score, int time) throws IOException {
-
-        Element levelElement = new Element(type);
+        Element levelElement = new Element(controller.type);
         Document doc = new Document(levelElement);
 
-        Element parent = new Element("numberOfLives");
-        parent.setText(Integer.toString(numberOfLives));
+        Element parent = new Element("lives");
+        parent.setText(Integer.toString(controller.lives));
         levelElement.addContent(parent);
 
         parent = new Element("difficulty");
-        parent.setText(Integer.toString(difficulty));
+        parent.setText(Integer.toString(controller.difficulty));
         levelElement.addContent(parent);
 
-        parent = new Element("score");
-        parent.setText(Integer.toString(score));
+        parent = new Element("scores");
+        parent.addContent(new Element("score").setText(Integer.toString(controller.score)));
+        parent.addContent(new Element("personalHighscore").setText(Integer.toString(controller.personalHighscore)));
         levelElement.addContent(parent);
 
         parent = new Element("time");
-        parent.setText(Integer.toString(time));
-        levelElement.addContent(parent);
+        Element child = new Element("mins");
+        child.setText(Integer.toString(controller.mins));
+        parent.addContent(child);
+        child = new Element("secs");
+        child.setText(Integer.toString(controller.secs));
+        parent.addContent(child);
 
         parent = new Element("fruits");
-        Element child;
-        for (GameObject throwable : throwables) {
+        for (GameObject throwable : controller.throwables) {
             if (throwable instanceof Apple) {
                 parent.addContent("apple");
             }
@@ -77,7 +104,7 @@ public class FilesManegement {
                 parent.addContent("melon");
             }
             if (throwable instanceof FreezeBanana) {
-                parent.addContent("score2xBanana");
+                parent.addContent("freezeBanana");
             }
             if (throwable instanceof MagicBeans) {
                 parent.addContent("magicBeans");
@@ -88,24 +115,34 @@ public class FilesManegement {
             if (throwable instanceof FatalBomb) {
                 parent.addContent("fatalBomb");
             }
-            child = new Element("X");
+            child = new Element("x");
             child.setText(Double.toString(throwable.getXlocation()));
 
-            child = new Element("Y");
+            child = new Element("y");
             child.setText(Double.toString(throwable.getYlocation()));
 
             child = new Element("maxHeight");
             child.setText(Double.toString(throwable.getMaxHeight()));
 
-            child = new Element("initialVelocity");
-            child.setText(Integer.toString(throwable.getInitialVelocity()));
+            child = new Element("startPos");
+            child.setText(Double.toString(throwable.getStartPos()));
 
-            child = new Element("fallingVelocity");
-            child.setText(Integer.toString(throwable.getFallingVelocity()));
+            child = new Element("midPoint");
+            child.setText(Double.toString(throwable.getMidPoint()));
+
+            child = new Element("endPos");
+            child.setText(Double.toString(throwable.getEndPos()));
+            child = new Element("a");
+            child.setText(Double.toString(throwable.getA()));
+
+            child = new Element("p");
+            child.setText(Double.toString(throwable.getP()));
+
+            child = new Element("q");
+            child.setText(Double.toString(throwable.getQ()));
 
             child = new Element("falling");
             child.setText(Boolean.toString(throwable.isFalling()));
-
             child = new Element("sliced");
             child.setText(Boolean.toString(throwable.isSliced()));
 
@@ -118,53 +155,66 @@ public class FilesManegement {
 
         XMLOutputter xmlOutput = new XMLOutputter();
 
-        File file = new File(System.getProperty("user.home") + File.separator + "Documents" + File.separator + "Fruit Ninja Game/Saves");
+        File file = new File(System.getProperty("user.home") + File.separator + "Documents" + File.separator + "Fruit Ninja Game/Saves" + File.separator + controller.username + "/");
         if (file.mkdirs()) {
             file.createNewFile();
         }
         xmlOutput.setFormat(Format.getPrettyFormat());
-        xmlOutput.output(doc, new FileWriter(file + "/" + type + ".txt"));
+        xmlOutput.output(doc, new FileWriter(file + "/" + controller.type + ".txt"));
     }
 
-    public void load(String type) throws JDOMException, IOException {
-        File inputFile = new File(System.getProperty("user.home") + File.separator + "Documents" + File.separator + "Fruit Ninja Game/Saves" + File.separator + type + ".txt");
+    public void load(Controller controller, String username, String type) throws JDOMException, IOException {
+        File inputFile = new File(System.getProperty("user.home") + File.separator + "Documents" + File.separator + "Fruit Ninja Game/Saves" + File.separator + username + File.separator + type + ".txt");
         SAXBuilder saxBuilder = new SAXBuilder();
         Document document = saxBuilder.build(inputFile);
         Element classElement = document.getRootElement();
 
-        numberOfLives = Integer.parseInt(classElement.getChild("numberOfLives").getText());
-        difficulty = Integer.parseInt(classElement.getChild("difficulty").getText());
-        score = Integer.parseInt(classElement.getChild("score").getText());
-        time = Integer.parseInt(classElement.getChild("time").getText());
+        controller.lives = Integer.parseInt(classElement.getChild("lives").getText());
+        controller.difficulty = Integer.parseInt(classElement.getChild("difficulty").getText());
+
+        controller.score = Integer.parseInt(classElement.getChild("scores").getChild("score").getText());
+        controller.personalHighscore = Integer.parseInt(classElement.getChild("scores").getChild("personalHighscore").getText());
+
+        controller.mins = Integer.parseInt(classElement.getChild("time").getChild("mins").getText());
+
+        controller.secs = Integer.parseInt(classElement.getChild("time").getChild("secs").getText());
 
         List<Element> childs = classElement.getChildren("fruits");
         for (int i = 0; i < childs.size(); i++) {
             Element child = childs.get(i);
-            switch (child.getName()) {
-//                case "apple":
-//                    throwables.add(new Apple(Double.parseDouble(child.getChild("X").getText()), Double.parseDouble(child.getChild("Y").getText()), Double.parseDouble(child.getChild("maxHeight").getText()), Integer.parseInt(child.getChild("initialVelocity").getText()), Integer.parseInt(child.getChild("fallingVelocity").getText()), Boolean.parseBoolean(child.getChild("falling").getText()), Boolean.parseBoolean(child.getChild("sliced").getText()), Boolean.parseBoolean(child.getChild("movedOffScreen").getText())));
-//                case "banana":
-//                    throwables.add(new Banana(Double.parseDouble(child.getChild("X").getText()), Double.parseDouble(child.getChild("Y").getText()), Double.parseDouble(child.getChild("maxHeight").getText()), Integer.parseInt(child.getChild("initialVelocity").getText()), Integer.parseInt(child.getChild("fallingVelocity").getText()), Boolean.parseBoolean(child.getChild("falling").getText()), Boolean.parseBoolean(child.getChild("sliced").getText()), Boolean.parseBoolean(child.getChild("movedOffScreen").getText())));
-//                case "orange":
-//                    throwables.add(new Orange(Double.parseDouble(child.getChild("X").getText()), Double.parseDouble(child.getChild("Y").getText()), Double.parseDouble(child.getChild("maxHeight").getText()), Integer.parseInt(child.getChild("initialVelocity").getText()), Integer.parseInt(child.getChild("fallingVelocity").getText()), Boolean.parseBoolean(child.getChild("falling").getText()), Boolean.parseBoolean(child.getChild("sliced").getText()), Boolean.parseBoolean(child.getChild("movedOffScreen").getText())));
-//                case "melon":
-//                    throwables.add(new Melon(Double.parseDouble(child.getChild("X").getText()), Double.parseDouble(child.getChild("Y").getText()), Double.parseDouble(child.getChild("maxHeight").getText()), Integer.parseInt(child.getChild("initialVelocity").getText()), Integer.parseInt(child.getChild("fallingVelocity").getText()), Boolean.parseBoolean(child.getChild("falling").getText()), Boolean.parseBoolean(child.getChild("sliced").getText()), Boolean.parseBoolean(child.getChild("movedOffScreen").getText())));
-//                case "magicBeans":
-//                    throwables.add(new MagicBeans(Double.parseDouble(child.getChild("X").getText()), Double.parseDouble(child.getChild("Y").getText()), Double.parseDouble(child.getChild("maxHeight").getText()), Integer.parseInt(child.getChild("initialVelocity").getText()), Integer.parseInt(child.getChild("fallingVelocity").getText()), Boolean.parseBoolean(child.getChild("falling").getText()), Boolean.parseBoolean(child.getChild("sliced").getText()), Boolean.parseBoolean(child.getChild("movedOffScreen").getText())));
-//                case "score2xBanana":
-//                    throwables.add(new FreezeBanana(Double.parseDouble(child.getChild("X").getText()), Double.parseDouble(child.getChild("Y").getText()), Double.parseDouble(child.getChild("maxHeight").getText()), Integer.parseInt(child.getChild("initialVelocity").getText()), Integer.parseInt(child.getChild("fallingVelocity").getText()), Boolean.parseBoolean(child.getChild("falling").getText()), Boolean.parseBoolean(child.getChild("sliced").getText()), Boolean.parseBoolean(child.getChild("movedOffScreen").getText())));
-//                case "dangerousBomb":
-//                    throwables.add(new DangerousBomb(Double.parseDouble(child.getChild("X").getText()), Double.parseDouble(child.getChild("Y").getText()), Double.parseDouble(child.getChild("maxHeight").getText()), Integer.parseInt(child.getChild("initialVelocity").getText()), Integer.parseInt(child.getChild("fallingVelocity").getText()), Boolean.parseBoolean(child.getChild("falling").getText()), Boolean.parseBoolean(child.getChild("sliced").getText()), Boolean.parseBoolean(child.getChild("movedOffScreen").getText())));
-//                case "fatalBomb":
-//                    throwables.add(new FatalBomb(Double.parseDouble(child.getChild("X").getText()), Double.parseDouble(child.getChild("Y").getText()), Double.parseDouble(child.getChild("maxHeight").getText()), Integer.parseInt(child.getChild("initialVelocity").getText()), Integer.parseInt(child.getChild("fallingVelocity").getText()), Boolean.parseBoolean(child.getChild("falling").getText()), Boolean.parseBoolean(child.getChild("sliced").getText()), Boolean.parseBoolean(child.getChild("movedOffScreen").getText())));
+            double x = Double.parseDouble(child.getChild("x").getText());
+            double y = Double.parseDouble(child.getChild("y").getText());
+            double maxHeight = Double.parseDouble(child.getChild("maxHeight").getText());
+            double startPos = Double.parseDouble(child.getChild("startPos").getText());
+            double midPoint = Double.parseDouble(child.getChild("midPoint").getText());
+            double endPos = Double.parseDouble(child.getChild("endPos").getText());
+            double a = Double.parseDouble(child.getChild("a").getText());
+            double p = Double.parseDouble(child.getChild("p").getText());
+            double q = Double.parseDouble(child.getChild("q").getText());
+            boolean falling = Boolean.parseBoolean(child.getChild("falling").getText());
+            boolean sliced = Boolean.parseBoolean(child.getChild("sliced").getText());
+            boolean movedOffScreen = Boolean.parseBoolean(child.getChild("movedOffScreen").getText());
+            GameObject object = createObject(child.getName());
 
-            }
+            object.setX(x);
+            object.setY(y);
+            object.setMaxHeight(maxHeight);
+            object.setStartPos(startPos);
+            object.setMidPoint(midPoint);
+            object.setEndPos(endPos);
+            object.setA(a);
+            object.setP(p);
+            object.setQ(q);
+            object.setFalling(falling);
+            object.setSliced(sliced);
+            object.setMovedOffScreen(movedOffScreen);
+            controller.throwables.add(object);
         }
     }
 
-    public void saveHighScore(int highScore) throws IOException {
-        Element levelElement = new Element("highScore");
-        levelElement.setText(Integer.toString(highScore));
+    public void saveHighScore(Controller controller) throws IOException {
+        Element levelElement = new Element("highestScore");
+        levelElement.setText(Integer.toString(controller.highestScore));
         Document doc = new Document(levelElement);
         XMLOutputter xmlOutput = new XMLOutputter();
 
@@ -173,37 +223,13 @@ public class FilesManegement {
             file.createNewFile();
         }
         xmlOutput.setFormat(Format.getPrettyFormat());
-        xmlOutput.output(doc, new FileWriter(file + "/highScore.txt"));
+        xmlOutput.output(doc, new FileWriter(file + "/highestScore.txt"));
     }
 
-    public void loadHighScore() throws JDOMException, IOException {
+    public void loadHighScore(Controller controller) throws JDOMException, IOException {
         File inputFile = new File(System.getProperty("user.home") + File.separator + "Documents" + File.separator + "Fruit Ninja Game/LeaderBoards" + File.separator + "highScore.txt");
         SAXBuilder saxBuilder = new SAXBuilder();
         Document document = saxBuilder.build(inputFile);
-        highScore = Integer.parseInt(document.getRootElement().getText());
-    }
-
-    public List<GameObject> getThrowables() {
-        return throwables;
-    }
-
-    public int getNumberOfLives() {
-        return numberOfLives;
-    }
-
-    public int getDifficulty() {
-        return difficulty;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public int getTime() {
-        return time;
-    }
-
-    public int getHighScore() {
-        return highScore;
+        controller.highestScore = Integer.parseInt(document.getRootElement().getText());
     }
 }
