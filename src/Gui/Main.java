4e8ad7ad968d9d2885446d46f7f685512d985fc6
Mostafa Talaat <mostafatalaat770@gmail.com>
2019-05-18 21:06:10 +0200

    MainMenu: refractor
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/Gui/Main.java
+++ src/Gui/Main.java
@@ -29,7 +29,7 @@ public class Main extends Application {
     }
 
     public void getMainMenu(MouseEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("FxmlFiles/StartScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FxmlFiles/MainMenu.fxml"));
         Scene classicScene = new Scene(root, 1270, 720);
         Stage classicWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
         classicWindow.setScene(classicScene);
@@ -40,7 +40,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FxmlFiles/StartScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FxmlFiles/MainMenu.fxml"));
         primaryStage.setTitle("Fruits Ninja");
         primaryStage.setScene(new Scene(root, 1270, 720));
         primaryStage.show();
