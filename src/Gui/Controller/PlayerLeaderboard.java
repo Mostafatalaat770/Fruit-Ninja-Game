69737882ecddc716722bba4625a17695d7f1591c
ahmed-swidan <48591254+ahmed-swidan@users.noreply.github.com>
2019-05-20 22:09:11 +0200

    playerLeaderboard added

---
+++ src/Gui/Controller/PlayerLeaderboard.java
@@ -0,0 +1,28 @@
+package Gui.Controller;
+
+public class PlayerLeaderboard {
+    String username;
+    int score;
+
+
+
+    public PlayerLeaderboard(String username, int score) {
+        this.username = username;
+        this.score = score;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
