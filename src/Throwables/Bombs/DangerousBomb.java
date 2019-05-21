f77ddf008e41f09bacc426acd88cb4959c5f512c
seifamr17 <seifamr17@gmail.com>
2019-05-21 04:53:20 +0200

    added exploded dangerous bomb

--- src/Throwables/Bombs/DangerousBomb.java
+++ src/Throwables/Bombs/DangerousBomb.java
@@ -12,7 +12,7 @@ public class DangerousBomb extends Bomb {
     public DangerousBomb() {
         super();
         super.setImg1(new Image("Resources/-10_Bomb.png", 75, 75, true, true));
-        super.setImg2(new Image("Resources/-10_Bomb.png", 75, 75, true, true));
+        super.setImg2(new Image("file:-10_Bomb_Sliced.png", 75, 75, true, true));
     }
 
     @Override
